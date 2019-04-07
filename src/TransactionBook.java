70f6bcd5ed9bcde2e3b289ceeabb61ad1fa2a029
bennowotny <ben@hortonvillerobotics.com>
2019-04-07 15:00:30 -0500

    Commented TransactionBook

--- src/TransactionBook.java
+++ src/TransactionBook.java
@@ -5,6 +5,8 @@ import java.util.Comparator;
 public class TransactionBook {
     ArrayList<Transaction> transactions = new ArrayList<>();
 
+
+    //Takes in a string of transactions and populates the list of transactions from it
     public void saveTransactions(String in){
         String[] ts = in.split("\n");
         for(String s : ts){
@@ -12,6 +14,7 @@ public class TransactionBook {
             String[] item = s.split("\\[")[1].split(",");
             transactions.add(new Transaction(fields[0]+","+fields[1],new Item(item[0], Double.parseDouble(item[1]), Integer.parseInt(item[2].substring(0,item[2].length()-1))), fields[fields.length-1]));
         }
+        //Sorts the transactions by time
         transactions.sort(new Comparator<Transaction>() {
             @Override
             public int compare(Transaction o1, Transaction o2) {
@@ -21,6 +24,7 @@ public class TransactionBook {
 
     }
 
+    //Return all unique item types in the record
     public String[] getUniqueItems(){
         if(transactions.size() == 0) return null;
         ArrayList<String> types = new ArrayList<>();
@@ -30,8 +34,8 @@ public class TransactionBook {
         return types.toArray(new String[types.size()]);
     }
 
+    //Return all Transactions for a given product
     public ArrayList<Transaction> getTypedTransactions(String name){
-        //if(Arrays.binarySearch(getUniqueItems(), name) == -1) return null;
         ArrayList<Transaction> ts = new ArrayList<>();
         for(Transaction t : transactions){
             if(t.item.name.equals(name)) ts.add(t);
